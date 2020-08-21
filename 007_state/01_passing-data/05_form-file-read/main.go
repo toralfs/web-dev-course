c7aba3ab3106994dfe88db0c22b988f83e2d14db
toralfs <toralfs97@gmail.com>
2020-08-21 23:11:06 +0200

    started state topic

---
+++ 007_state/01_passing-data/05_form-file-read/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	var s string
+	if req.Method == http.MethodPost {
+		// open
+		f, h, err := req.FormFile("q")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+
+		// for my information
+		fmt.Println("\nfile:", f, "\nheader:", h, "\nerr:", err)
+
+		// read
+		bs, err := ioutil.ReadAll(f)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		s = string(bs)
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `
+	<form method="POST" enctype="multipart/form-data">
+	<input type="file" name="q">
+	<input type="submit">
+	</form>
+	<br>`+s)
+}
